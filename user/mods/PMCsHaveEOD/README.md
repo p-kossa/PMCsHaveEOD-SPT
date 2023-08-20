4c62d9a0c87e0b51709dc0d74c451bad36645325
p-kossa <kossa526+git@gmail.com>
2023-08-20 14:15:10 -0400

    try postdb load

--- user/mods/PMCsHaveEOD/README.md
+++ user/mods/PMCsHaveEOD/README.md
@@ -1,1 +1,7 @@
 # PMCsHaveEOD-SPT
+
+This is an update of JustNU's [PMC Bots Have EOD Account](https://hub.sp-tarkov.com/files/file/991-pmc-bots-have-eod-account/) mod for SPT 3.6.x. All credit goes to JustNU.
+
+Simple mod that makes all PMC bots have EOD accounts for a little bit of extra immersion.
+
+Should be compatabile with anything.
--- user/mods/PMCsHaveEOD/src/mod.ts
+++ user/mods/PMCsHaveEOD/src/mod.ts
@@ -3,72 +3,31 @@ import { IBotConfig } from "@spt-aki/models/spt/config/IBotConfig";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { ConfigServer } from "@spt-aki/servers/ConfigServer";
 import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
-import { HttpResponseUtil } from "@spt-aki/utils/HttpResponseUtil";
-import { StaticRouterModService } from "@spt-aki/services/mod/staticRouter/StaticRouterModService";
 
-const modName = "PMCsHaveEOD";
 let logger: ILogger;
 let configServer: ConfigServer;
 let botConfig: IBotConfig;
 
 
 class Mod {
-  preAkiLoad(container: DependencyContainer): void {
-    const HttpResponse = container.resolve<HttpResponseUtil>("HttpResponseUtil");
-    const staticRouterModService = container.resolve<StaticRouterModService>(
-      "StaticRouterModService"
-    );
-
-    staticRouterModService.registerStaticRouter(
-      `${modName}:/client/raid/configuration`,
-      [
-        {
-          url: "/client/raid/configuration",
-          action: (
-            url: string,
-            info: any,
-            sessionID: string,
-            output: string
-          ): any => {
-
-            try {
-              const bots: IBotConfig = configServer.getConfig(
-                ConfigTypes.BOT
-              );
-
-              logger.info(bots.pmc.accountTypeWeight)
-
-              bots.pmc.gameVersionWeight["standard"] = 0
-              bots.pmc.gameVersionWeight["left_behind"] = 0
-              bots.pmc.gameVersionWeight["prepare_for_escape"] = 0
-              bots.pmc.gameVersionWeight["edge_of_darkness"] = 100
-          
-              bots.pmc.accountTypeWeight["0"] = 0
-              bots.pmc.accountTypeWeight["1"] = 0
-              bots.pmc.accountTypeWeight["2"] = 100
-              bots.pmc.accountTypeWeight["256"] = 0
-              bots.pmc.accountTypeWeight["512"] = 0
-
-              return HttpResponse.nullResponse();
-            }
-            catch (e) {
-              logger.info(e)
-              return HttpResponse.nullResponse();
-            }
-          }
-        }
-      ],
-      "PMCsHaveEOD"
-    );
-  }
-
   postDBLoad(container: DependencyContainer): void {
     logger = container.resolve<ILogger>("WinstonLogger");
     configServer = container.resolve<ConfigServer>("ConfigServer");
     botConfig = configServer.getConfig<IBotConfig>(ConfigTypes.BOT);
+
+    const bots: IBotConfig = configServer.getConfig(
+      ConfigTypes.BOT
+    );
+
+    bots.pmc.gameVersionWeight = {
+      "edge_of_darkness": 100
+    }
+
+    bots.pmc.accountTypeWeight = {
+      "2": 100
+    }
   }
 }
-
 
 
 module.exports = { mod: new Mod() }
