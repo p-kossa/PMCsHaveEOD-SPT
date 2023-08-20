110c99e95fef84448563ed2d7c37dba1db2c8840
p-kossa <kossa526+git@gmail.com>
2023-08-20 12:09:31 -0400

    test

---
+++ user/mods/PMCsHaveEOD/src/mod.ts
@@ -0,0 +1,28 @@
+﻿import { IBotConfig } from "@spt-aki/models/spt/config/IBotConfig";
+import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+
+let logger: ILogger;
+
+class Mod {
+	postDBLoad(container) {
+    const bots: IBotConfig = configServer.getConfig(
+      ConfigTypes.BOT
+    );
+
+    bots.pmc.gameVersionWeight["standard"] = 0
+    bots.pmc.gameVersionWeight["left_behind"] = 0
+    bots.pmc.gameVersionWeight["prepare_for_escape"] = 0
+    bots.pmc.gameVersionWeight["edge_of_darkness"] = 100
+
+		bots.pmc.accountTypeWeight["0"] = 0
+		bots.pmc.accountTypeWeight["1"] = 0
+		bots.pmc.accountTypeWeight["2"] = 100
+		bots.pmc.accountTypeWeight["256"] = 0
+		bots.pmc.accountTypeWeight["512"] = 0
+
+    logger.info("PMCsHaveEOD: Values updated, all PMCs should have EOD now")
+	}
+}
+
+
+module.exports = { mod: new Mod() }
